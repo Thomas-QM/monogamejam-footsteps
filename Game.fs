b048d9966016c5595db6ba198141b7c18216559a
Thomas-QM <thomas@engineeringvirtue.net>
2018-07-27 23:20:28 -0700

    Changed to dotnet core there isnt actually anything

---
+++ Game.fs
@@ -0,0 +1,39 @@
+﻿namespace FSGame
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type MainGame () as x =
+    inherit Game()
+
+    do x.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(x)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    override x.Initialize() =
+        do spriteBatch <- new SpriteBatch(x.GraphicsDevice)
+        do base.Initialize()
+        x.IsMouseVisible <- false
+
+         // TODO: Add your initialization logic here
+
+        ()
+
+    override x.LoadContent() =
+
+         // TODO: use this.Content to load your game content here
+
+        ()
+
+    override x.Update (gameTime) =
+
+         // TODO: Add your update logic here
+
+        ()
+
+    override x.Draw (gameTime) =
+        do x.GraphicsDevice.Clear Color.CornflowerBlue
+
+        // TODO: Add your drawing code here
+
+        ()
---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿module Program
+
+[<EntryPoint>]
+let main argv =
+    let game = new FSGame.MainGame()
+    game.Run ()
+    0
